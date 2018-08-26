67a3c24660422335eb57279996beb2bf4f45c4d2
Tobias Haugen <hej@tobiashaugen.se>
2018-08-26 12:17:24 +0200

    Kept exploring with grunt concat and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,49 @@
 module.exports = function(grunt) {
+	// Project configuration.
 	grunt.initConfig({
+		// Project metadata is imported into the Grunt config. This allows us to
+		// refer to the values of properties within our package.json file.
 		pkg: grunt.file.readJSON('package.json'),
+		// Configuration for Sass plugin.
 		sass: {
 			dist: {
 				files: {
-					'style/style.css' : 'sass/style.scss'
+					'style/style.css' : 'sass/style.scss',
+					'style/second.css' : 'sass/second.scss'
 				}
 			}
 		},
+		// The Concat plugin concatinates files into one.
+		concat: {
+		  options: {
+		    // Concatenated files will be joined on this string.
+		    separator: '\n'
+		  },
+		  dist: {
+		    // The files to concatenate. All css files in the styles folder.
+		    src: ['style/*.css'],
+		    // The location of the resulting concatinated css file. Using pkg.name
+				// here as an example. Refers to the "name" property in package.json.
+		    dest: 'style/prod/<%= pkg.name %>.css'
+		  }
+		},
+		// The Watch plugin Runs a task whenever files are added, changed or
+		// deleted.
 		watch: {
 			css: {
 				files: '**/*.scss',
+				// The task to run when prompted, in this case "sass".
 				tasks: ['sass']
 			}
 		}
 	});
+
+	// Load Grunt plugins.
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	// Set the default task to "watch", so running "grunt" will be equal to
+	// running "grunt watch"
 	grunt.registerTask('default',['watch']);
 }
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Grunt, Compass and SASS</title>
+  <link rel="stylesheet" type="text/css" href="style/prod/gcs.css">
+</head>
+<body>
+
+    <h1>Test page</h1>
+
+</body>
+</html>
