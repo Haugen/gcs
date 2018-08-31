fe122dcbf9f11ccf88818be2520d7d54dcb40649
Tobias Haugen <hej@tobiashaugen.se>
2018-08-31 13:29:48 +0200

    Played with Bootstrap Cards.

--- album/index.php
+++ album/index.php
@@ -25,22 +25,160 @@
         </nav>
         <form class="form-inline">
           <input type="search" class="form-control mr-sm-2">
-          <button type="submit" class="btn btn-outline-info my-2 my-sm-0">Search</button>
+          <button type="submit" class="btn btn-outline-primary my-2 my-sm-0">Search</button>
         </form>
       </div>
     </div>
   </header>
 
   <main>
+    <!-- Jumbotron section. -->
     <section class="jumbotron jumbotron-fluid text-center">
       <div class="container">
         <h1 class="display-4">My favorite albums</h1>
         <p class="lead text-muted">These are my 9 favorite albums as of fall 2018.</p>
         <hr class="my-4" />
-        <button class="btn btn-outline-info btn-lg btn-rounded">Spotify Playlist</button>
+        <button class="btn btn-outline-primary btn-lg btn-rounded">Spotify Playlist</button>
       </div>
     </section>
+
+    <!-- Cards grid section. -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist. Short album information or maybe even something about the artist. Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist. Short album information or maybe even something about the artist. Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist. Short album information or maybe even something about the artist. Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img class="card-img-top" src="img/thumb.png" alt="Thumbnail" />
+            <div class="card-body">
+              <h5 class="card-title">Album title</h5>
+              <h6 class="card-subtitle text-muted mb-2">by Artist name</h6>
+              <p class="card-text">Short album information or maybe even something about the artist.</p>
+              <a href="#" class="card-link">On Spotify</a>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Alternative Rock</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
+
+  <footer class="text-center py-5 bg-dark text-light mt-5">
+    Favorite Spotify albums 2018
+    <small class="text-muted d-block">Practice makes perfect.</small>
+  </footer>
 
   <script src="../node_modules/jquery/dist/jquery.min.js"></script>
   <script src="../node_modules/popper.js/dist/umd/popper.min.js"></script>
